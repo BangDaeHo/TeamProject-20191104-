bca62c97fce2c4e15215b8ce80ecd27c1a954bcc
BangDaeHo <hanava1@naver.com>
2019-11-04 14:16:06 +0900

    MyMouseListener 작성

--- MyMouseListener.java
+++ MyMouseListener.java
@@ -13,24 +13,33 @@ public class MyMouseListener extends MouseMotionAdapter implements MouseListener
     public MyMouseListener(JLabel ml){
         this.ml = ml;
     }
-    
+
     public void mouseClicked(MouseEvent e){}
+
     public void mouseEntered(MouseEvent e){
         Component c = (Component)e.getSource();
         c.setBackground(Color.BLUE);
         c.setBackground(Color.CYAN);
     }
+
     public void mouseExited(MouseEvent e){
         Component c = (Component)e.getSource();
         c.setBackground(Color.YELLOW);
     }
+
     public void mousePressed(MouseEvent e){
         ml.setText("MousePressed" + "(" + ml.getX() + ", " + ml.getY() + ")");
     }
+
     public void mouseReleased(MouseEvent e){
         ml.setText("MouseReleased" + "(" + ml.getX() + ", " + ml.getY() + ")");
     }
-    
-    public void mouseDragged(MouseEvent e){}
-    public void mouseMoved(MouseEvent e){}
+
+    public void mouseDragged(MouseEvent e){
+        ml.setText("MouseReleased" + "(" + ml.getX() + ", " + ml.getY() + ")");
+    }
+
+    public void mouseMoved(MouseEvent e){
+         ml.setText("MouseReleased" + "(" + ml.getX() + ", " + ml.getY() + ")");
+    }
 }
